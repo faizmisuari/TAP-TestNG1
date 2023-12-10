63bd915f9bdef542ec1571973a34db69970423c3
Faiz Misuari <mailtofaizmisuari@gmail.com>
2023-12-10 23:03:20 +0800

    testrun

--- scripts/testng/src/test/java/Googletest/GmailTest.java
+++ scripts/testng/src/test/java/Googletest/GmailTest.java
@@ -1,5 +1,16 @@
 package Googletest;
 
-public abstract class GmailTest {
+import org.junit.Test;
 
+public abstract class GmailTest {
+    @Test
+    public void LoginTest()
+    {
+    System.out.println("Successfully Logged In");
+    }
+    @Test
+    public void LogoutTest()
+    {
+    System.out.println("Successfully Logged Out");
+    }
 }
